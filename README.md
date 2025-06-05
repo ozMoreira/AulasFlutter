51c848cb998b9ccb770740035ffb7bc5dfa53372
Oswaldo Moreira <oz.moreira@icloud.com>
2025-06-05 18:47:37 -0300

    Setup do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# web01
+
+A new Flutter project.
---
+++ lib/Pages/myhomepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(title: const Text('My Home Page')),
+      body: Center(child: const Text('Teste da pagina 1')),
+    );
+  }
+}

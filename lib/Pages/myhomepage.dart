d82ff6a7da8957d8fa94451ad4133d2175ad3b72
Oswaldo Moreira <oz.moreira@icloud.com>
2025-06-05 18:48:32 -0300

    criando primeira pagina

--- lib/Pages/myhomepage.dart
+++ lib/Pages/myhomepage.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 400,
+          color: Colors.yellow,
+          child: Center(child: const Text('Teste da pagina 1')),
+        ),
+      ),
       backgroundColor: Colors.blue,
       appBar: AppBar(title: const Text('My Home Page')),
-      body: Center(child: const Text('Teste da pagina 1')),
     );
   }
 }
